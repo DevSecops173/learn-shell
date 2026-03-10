97f19250ca413285bf58753bd704cb451f426a10
Gopichand173 <gopi110531@gmail.com>
2026-03-10 04:21:48 -0500

    roboshop code

---
+++ function.sh
@@ -0,0 +1,12 @@
+#if we  assign a name to set of commands then it is a function
+#syntex
+#function function_name(){
+#commands
+#}
+
+sample(){
+  echo sample function
+}
+#call a function
+#funtion_name
+sample
